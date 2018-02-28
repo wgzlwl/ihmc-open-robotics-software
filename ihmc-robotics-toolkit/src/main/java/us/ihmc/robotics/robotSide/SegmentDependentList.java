602eecf74e872d3acc7a1afb31bc8ac64c983cdc
Robert <rgriffin@ihmc.us>
2018-02-28 10:08:50 -0600

    cleaned up a unified side dependent lists and such

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/robotSide/SegmentDependentList.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/robotSide/SegmentDependentList.java
@@ -4,12 +4,11 @@ import java.util.*;
 
 public class SegmentDependentList<E extends Enum<E> & RobotSegment<E>, V> extends EnumMap<E, V>
 { 
-
    public SegmentDependentList(Class<E> keyClass)
    {
       super(keyClass);
    }
-   
+
    public V get(E key)
    {
       return super.get(key);
@@ -18,11 +17,5 @@ public class SegmentDependentList<E extends Enum<E> & RobotSegment<E>, V> extend
    public V set(E segment, V element)
    {
       return this.put(segment, element);
-   }
-
-
-   public boolean containsSegment(E robotSegment)
-   {
-      return this.containsKey(robotSegment);
    }
 }
