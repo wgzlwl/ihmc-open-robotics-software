5e6ef31fa7ea689eb420b9e452e7b0f8dc355a79
InhoLee <ilee@ihmc.us>
2018-03-13 12:10:47 -0500

    Two falling box robot simulation is initiated.

---
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/fallingBox/FallingBoxRobotSimulation.java
@@ -0,0 +1,93 @@
+package us.ihmc.exampleSimulations.fallingBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import us.ihmc.euclid.geometry.Box3D;
+import us.ihmc.graphicsDescription.Graphics3DObject;
+import us.ihmc.simulationConstructionSetTools.util.environments.FlatGroundEnvironment;
+import us.ihmc.simulationconstructionset.FloatingJoint;
+import us.ihmc.simulationconstructionset.GroundContactModel;
+import us.ihmc.simulationconstructionset.Robot;
+import us.ihmc.simulationconstructionset.RobotFromDescription;
+import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+import us.ihmc.simulationconstructionset.SimulationConstructionSetParameters;
+import us.ihmc.simulationconstructionset.physics.CollisionHandler;
+import us.ihmc.simulationconstructionset.physics.collision.DefaultCollisionHandler;
+import us.ihmc.simulationconstructionset.physics.collision.DefaultCollisionVisualizer;
+import us.ihmc.simulationconstructionset.util.LinearGroundContactModel;
+
+public class FallingBoxRobotSimulation
+{
+   private static final Box3D box = new Box3D(0.5, 0.3, 0.1);
+   private static final double mass = 10.0;
+
+   private static final double height = 1.0;
+   private static final double width = 1.0;
+
+   public FallingBoxRobotSimulation()
+   {
+      double dt = 0.001;
+
+      BoxRobotDescription gcpRobotDescription = new BoxRobotDescription("gcpRobot", box, mass, true);
+      BoxRobotDescription csRobotDescription = new BoxRobotDescription("csRobot", box, mass, false);
+      BoxRobotCollisionMeshDefinitionDataHolder collisionMeshData = new BoxRobotCollisionMeshDefinitionDataHolder(box);
+      csRobotDescription.addCollisionMeshDefinitionData(collisionMeshData);
+
+      Robot gcpRobot = new RobotFromDescription(gcpRobotDescription);
+      Robot csRobot = new RobotFromDescription(csRobotDescription);
+      
+      GroundContactModel groundModel = new LinearGroundContactModel(gcpRobot, 1422, 15.6, 125.0, 300.0, gcpRobot.getRobotsYoVariableRegistry()); // same value with DRCSCSInitialSetup.
+      gcpRobot.setGroundContactModel(groundModel);
+
+      FloatingJoint gcpFloatingJoint = (FloatingJoint) gcpRobot.getRootJoints().get(0);
+      gcpFloatingJoint.setPosition(0.0, 0.5 * width, height);
+      gcpFloatingJoint.setVelocity(1.0, 0.0, 0.0);
+      FloatingJoint csFloatingJoint = (FloatingJoint) csRobot.getRootJoints().get(0);
+      csFloatingJoint.setPosition(0.0, -0.5 * width, height);
+      csFloatingJoint.setVelocity(1.0, 0.0, 0.0);
+
+      GroundContactPointBasedEstimator gcpEstimator = new GroundContactPointBasedEstimator(gcpRobot);
+      gcpRobot.setController(gcpEstimator);
+      CollisionShapeBasedEstimator csEstimator = new CollisionShapeBasedEstimator(csRobot);
+      csRobot.setController(csEstimator);
+
+      List<Robot> allSimulatedRobotList = new ArrayList<Robot>();
+      allSimulatedRobotList.add(gcpRobot);
+      allSimulatedRobotList.add(csRobot);
+
+      FlatGroundEnvironment environment = new FlatGroundEnvironment();
+
+      SimulationConstructionSetParameters parameters = new SimulationConstructionSetParameters();
+      parameters.setCreateGUI(true);
+      parameters.setDataBufferSize(8000);
+
+      SimulationConstructionSet scs = new SimulationConstructionSet(allSimulatedRobotList.toArray(new Robot[0]), parameters);
+      Graphics3DObject staticLinkGraphics = new Graphics3DObject();
+      staticLinkGraphics.addCoordinateSystem(0.1);
+      scs.addStaticLinkGraphics(staticLinkGraphics);
+      scs.addStaticLinkGraphics(environment.getTerrainObject3D().getLinkGraphics());
+
+      // simulate.
+      DefaultCollisionVisualizer collisionVisualizer = new DefaultCollisionVisualizer(100.0, 100.0, 0.01, scs, 1000);
+      double coefficientOfRestitution = 0.2;
+      double coefficientOfFriction = 0.7;
+      CollisionHandler collisionHandler = new DefaultCollisionHandler(coefficientOfRestitution, coefficientOfFriction);
+      scs.initializeCollisionDetector(collisionVisualizer, collisionHandler);
+      scs.addEnvironmentCollisionShapes(environment.getTerrainObject3D().getTerrainCollisionShapes());
+      scs.initializeCollisionHandler(collisionVisualizer, collisionHandler);
+
+      scs.setDT(dt, 1);
+      scs.setFastSimulate(true);
+      scs.setGroundVisible(false);
+
+      scs.setCameraFix(0.0, 0.0, 0.8);
+      scs.setCameraPosition(0.0, -8.0, 1.4);
+      scs.startOnAThread();
+   }
+
+   public static void main(String[] args)
+   {
+      new FallingBoxRobotSimulation();
+   }
+}
