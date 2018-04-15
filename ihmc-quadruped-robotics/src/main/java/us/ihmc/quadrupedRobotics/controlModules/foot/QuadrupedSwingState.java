ed74abdf2f39e8c37f0eb9492ff15bd96e265d75
Robert <rgriffin@ihmc.us>
2018-04-14 19:31:30 -0500

    cleaned up the swing state

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/controlModules/foot/QuadrupedSwingState.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/controlModules/foot/QuadrupedSwingState.java
@@ -147,7 +147,7 @@ public class QuadrupedSwingState extends QuadrupedFootState
       desiredSolePosition.setMatchingFrame(desiredPosition);
       desiredSoleLinearVelocity.setMatchingFrame(desiredVelocity);
 
-      // Detect early touch-down. // FIXME do something else with this (trigger a loaded transition?)
+      // Detect early touch-down.
       FrameVector3D soleForceEstimate = controllerToolbox.getTaskSpaceEstimates().getSoleVirtualForce(robotQuadrant);
       soleForceEstimate.changeFrame(worldFrame);
       double pressureEstimate = -soleForceEstimate.getZ();
@@ -157,8 +157,8 @@ public class QuadrupedSwingState extends QuadrupedFootState
          touchdownTrigger.update(pressureEstimate > parameters.getTouchdownPressureLimitParameter());
       }
 
-         feedbackControlCommand.set(desiredPosition, desiredVelocity);
-         feedbackControlCommand.setGains(parameters.getSolePositionGains());
+      feedbackControlCommand.set(desiredPosition, desiredVelocity);
+      feedbackControlCommand.setGains(parameters.getSolePositionGains());
 
       // Trigger support phase.
       if (currentTime >= touchDownTime)
@@ -222,7 +222,7 @@ public class QuadrupedSwingState extends QuadrupedFootState
    {
       if (swingIsDone)
          return QuadrupedFootControlModule.FootEvent.TIMEOUT;
-      if (touchdownTrigger.getBooleanValue() && timeInState > 0.5 * currentStepCommand.getTimeInterval().getDuration())
+      if (touchdownTrigger.getBooleanValue())
          return QuadrupedFootControlModule.FootEvent.LOADED;
 
       return null;
@@ -243,7 +243,7 @@ public class QuadrupedSwingState extends QuadrupedFootState
    @Override
    public PointFeedbackControlCommand getFeedbackControlCommand()
    {
-         return feedbackControlCommand;
+      return feedbackControlCommand;
    }
 
    @Override
